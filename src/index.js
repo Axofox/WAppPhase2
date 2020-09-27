22e1f1a965ab55db06dc123e690b6ee127a0182e
charnut <christine.harnut@gmail.com>
2020-09-27 23:51:23 +0200

    API added to city adn celsius

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-let temperature = 23;
 function currentDay(date) {
 	let currentWeekDay = date.toLocaleString("en-us", { weekday: "long" });
 	let currentDate = date.getDate();
@@ -12,18 +11,6 @@ function currentTime(time) {
 		time.getMinutes() < 10 ? "0" + time.getMinutes() : time.getMinutes();
 	document.getElementById("currentTime").innerHTML = `${hours}:${minutes}`;
 }
-function conversionToFahrenheit() {
-	document.getElementById("temp").innerHTML = `${Math.round(
-		(temperature * 9) / 5 + 32
-	)}°`;
-	document.getElementById("celsius-link").className = "link-darkGrey";
-	document.getElementById("fahrenheit-link").className = "link-orange";
-}
-function conversionToCelsius() {
-	document.getElementById("temp").innerHTML = `${temperature}°`;
-	document.getElementById("celsius-link").className = "link-orange";
-	document.getElementById("fahrenheit-link").className = "link-darkGrey";
-}
 
 function searchCity(event) {
 	event.preventDefault();
@@ -34,17 +21,37 @@ function searchCity(event) {
 }
 document.getElementById("form").addEventListener("submit", searchCity);
 
-document
-	.getElementById("fahrenheit-link")
-	.addEventListener("click", conversionToFahrenheit, false);
+// function conversionToFahrenheit() {
+// 	document.getElementById("temp").innerHTML = `${Math.round(
+// 		(temperature * 9) / 5 + 32
+// 	)}°`;
+// 	document.getElementById("celsius-link").className = "link-darkGrey";
+// 	document.getElementById("fahrenheit-link").className = "link-orange";
+// }
+// function conversionToCelsius() {
+// 	document.getElementById("temp").innerHTML = `${temperature}°`;
+// 	document.getElementById("celsius-link").className = "link-orange";
+// 	document.getElementById("fahrenheit-link").className = "link-darkGrey";
+// }
 
-document
-	.getElementById("celsius-link")
-	.addEventListener("click", conversionToCelsius, false);
-	
+// document
+// 	.getElementById("fahrenheit-link")
+// 	.addEventListener("click", conversionToFahrenheit, false);
+
+// document
+// 	.getElementById("celsius-link")
+// 	.addEventListener("click", conversionToCelsius, false);
+
+function displayTemp(response) {
+	let temperatureElem = document.getElementById("temp");
+	temperatureElem.innerHTML = Math.round(response.data.main.temp);
+	let myCityElem = document.getElementById("myCity");
+	myCityElem.innerHTML = response.data.name;
+}
+let temp = "11";
 let apiKey = "85f0d2edf77153a605301a461e1c1922";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Munich&appid=${apiKey}`;
-console.log(apiUrl);
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Munich&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemp);
 
 currentDay(new Date());
 
